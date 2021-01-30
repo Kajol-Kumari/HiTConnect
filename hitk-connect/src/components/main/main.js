af68b01c23314e1d720ebf4dc93a451fdeab29a9
kajol <kajolkumarisingh222@gmail.com>
2021-01-30 11:53:40 +0530

    Minor Fix

--- hitk-connect/src/components/main/main.js
+++ hitk-connect/src/components/main/main.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
 import {Switch,Route, withRouter } from 'react-router-dom'
 import Home from '../home/home';
-import Footer from '../template/footer/footer';
 import ClubsMenu from '../clubs-menu/clubs-menu';
 import NotFound from '../404/not_found';
 import LoginPage from '../auth/login-page/login-page';
 class Main extends Component {
-    constructor(props) {
-        super(props);
-    }
     render() {
         return (
             <div className="Container">
